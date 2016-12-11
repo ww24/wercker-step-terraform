c5568e03c13c5da336ced34cb7656a6e3097c3ed
Takenori Nakagawa <ww24gm@gmail.com>
2016-12-12 05:28:17 +0900

    use eval...

--- run.sh
+++ run.sh
@@ -10,8 +10,8 @@ terraform_cli="${WERCKER_STEP_ROOT}/terraform"
 
 if [ -n "${WERCKER_TERRAFORM_REMOTE_CONFIG}" ]; then
   WERCKER_TERRAFORM_REMOTE_CONFIG=$(echo "${WERCKER_TERRAFORM_REMOTE_CONFIG}" | sed -e 's/\\n/ /g')
-  $terraform_cli remote config "${WERCKER_TERRAFORM_REMOTE_CONFIG}"
+  eval "$terraform_cli remote config ${WERCKER_TERRAFORM_REMOTE_CONFIG}"
 fi
 
 WERCKER_TERRAFORM_COMMAND=$(echo "${WERCKER_TERRAFORM_COMMAND}" | sed -e 's/\\n/ /g')
-$terraform_cli "${WERCKER_TERRAFORM_COMMAND} $cli_args"
+eval "$terraform_cli ${WERCKER_TERRAFORM_COMMAND} $cli_args"
