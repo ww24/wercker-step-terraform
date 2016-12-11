9a69e8a791490907f06358e6a6f9c2d1a0e4f7fb
Takenori Nakagawa <ww24gm@gmail.com>
2016-12-12 05:22:02 +0900

    use sed

--- run.sh
+++ run.sh
@@ -9,9 +9,9 @@ fi
 terraform_cli="${WERCKER_STEP_ROOT}/terraform"
 
 if [ -n "${WERCKER_TERRAFORM_REMOTE_CONFIG}" ]; then
-  WERCKER_TERRAFORM_REMOTE_CONFIG=$(echo "${WERCKER_TERRAFORM_REMOTE_CONFIG}" | tr '\\n' ' ')
+  WERCKER_TERRAFORM_REMOTE_CONFIG=$(echo "${WERCKER_TERRAFORM_REMOTE_CONFIG}" | sed -e 's/\\n/ /g')
   $terraform_cli remote config "${WERCKER_TERRAFORM_REMOTE_CONFIG}"
 fi
 
-WERCKER_TERRAFORM_COMMAND=$(echo "${WERCKER_TERRAFORM_COMMAND}" | tr '\\n' ' ')
+WERCKER_TERRAFORM_COMMAND=$(echo "${WERCKER_TERRAFORM_COMMAND}" | sed -e 's/\\n/ /g')
 $terraform_cli "${WERCKER_TERRAFORM_COMMAND} $cli_args"
