d69f131cfb39fa50713521dd1f0a39aeeaea5992
Takenori Nakagawa <ww24gm@gmail.com>
2016-12-12 08:39:29 +0900

    initialize remote config

--- run.sh
+++ run.sh
@@ -9,15 +9,16 @@ fi
 terraform_cli="${WERCKER_STEP_ROOT}/terraform"
 
 if [ -n "${WERCKER_TERRAFORM_REMOTE_CONFIG}" ]; then
+  # initialize remote config
+  rm -rf .terraform
+
   WERCKER_TERRAFORM_REMOTE_CONFIG=${WERCKER_TERRAFORM_REMOTE_CONFIG//'\n'/' '}
-  eval "$terraform_cli remote config ${WERCKER_TERRAFORM_REMOTE_CONFIG}"
-  if [ "$?" -gt 0 ]; then
+  if ! eval "$terraform_cli remote config ${WERCKER_TERRAFORM_REMOTE_CONFIG}"; then
     fail "Invalid remote_config option"
   fi
 fi
 
 WERCKER_TERRAFORM_COMMAND=${WERCKER_TERRAFORM_COMMAND//'\n'/' '}
-eval "$terraform_cli ${WERCKER_TERRAFORM_COMMAND} $cli_args"
-if [ "$?" -gt 0 ]; then
+if ! eval "$terraform_cli ${WERCKER_TERRAFORM_COMMAND} $cli_args"; then
   fail "Invalid command option"
 fi
